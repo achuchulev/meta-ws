e3c083bdb27baca1d5e90d7fb64434aae69dc46e
Atanas Chuchulev <atanas.v4@gmail.com>
2023-07-14 14:59:56 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,26 @@ variable "my_org_name" {
   default = "atanas-free"
 }
 
+variable "vcs_identifier" {
+  default = "achuchulev/tf-null-count"
+}
+
 data "tfe_organization" "org" {
   name = var.my_org_name
+}
+
+data "tfe_oauth_client" "client" {
+  organization     = var.my_org_name
+  service_provider = "github"
 }
 
 resource "tfe_workspace" "test" {
   name         = "meta_producer"
   organization = data.tfe_organization.org.name
   tag_names    = ["meta", "workspaces"]
+
+  vcs_repo {
+    identifier     = var.vcs_identifier
+    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+  }
 }
